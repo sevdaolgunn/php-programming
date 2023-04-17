d9d2db0fb061ff7368dd07bcd723f23473944295
sevda <sevdaolgun111@hotmail.com>
2023-04-17 17:08:58 +0300

    added delete method

--- course3.php
+++ course3.php
@@ -9,7 +9,7 @@ $username = "root";
 $password = "";
 
 try {
-    $db = new PDO("mysql:host=$servername;dbname=medical", $username, $password);
+    $db = new PDO("mysql:host=$servername;dbname=university", $username, $password);
     // set the PDO error mode to exception
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
@@ -45,18 +45,48 @@ if ($_REQUEST['islem']=="ekle"){
     $fullname = $_REQUEST['fullname'];
     $department = $_REQUEST['department'];
     $city = $_REQUEST['city'];
-    $sql = "INSERT INTO university (fullname,department,city) VALUES ('$fullname', '$department','$city')";
+    $sql = "INSERT INTO students (fullname,department,city) VALUES ('$fullname', '$department','$city')";
     $db->exec($sql);
     echo "ADDED!!<br>";
+    header("Location: ?islem=eklendi");//boş kayıt eklenmesini engelleme
+}
+
+//SİLME KOMUTU
+if ($_REQUEST['islem']=="sil"){
+    $ID = $_REQUEST['ID'];
+    $sql = "DELETE FROM students WHERE ID=$ID";
+    $db->exec($sql);
+    header("Location: ?islem=silindi");
 }
 ?>
 *******ÖĞRENCİ LİSTESİ******<br>
+<table style="border:1px solid">
+    <tr>
+        <th>Full Name</th>
+        <th>Department</th>
+        <th>City</th>
+        <th>Delete</th>
+    </tr>
 <?php
 
-$sql="SELECT * FROM university";
-foreach ($db->query($sql) as $data )
-    echo $data['fullname']. "<br>";
+$sql="SELECT * FROM students";
+foreach ($db->query($sql) as $data ){
+
 
 ?>
+    <tr>
+        <td><?=$data['fullname']?></td>
+        <td><?=$data['department']?></td>
+        <td><?=$data['city']?></td>
+        <td><a href="?islem = sil&id=<?=$data['ID']?>">X</a></td>
+
+    </tr>
+    <?php
+    }
+?>
+
+
+
+</table>
 </body>
 </html>
