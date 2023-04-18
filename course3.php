f04e5d3199a7d53c657601f88b7307e5e5c96d70
sevda <sevdaolgun111@hotmail.com>
2023-04-19 01:54:44 +0300

    database operations(delete,edit, add)

--- course3.php
+++ course3.php
@@ -18,6 +18,42 @@ try {
 }
 ?>
 
+<?php
+//Ekleme Komutu
+if ($_REQUEST['islem']=="ekle"){
+    $fullname = $_REQUEST['fullname'];
+    $department = $_REQUEST['department'];
+    $city = $_REQUEST['city'];
+    $sql = "INSERT INTO students (fullname,department,city) VALUES ('$fullname', '$department','$city')";
+    $db->exec($sql);
+    echo "ADDED!!<br>";
+    header("Location: ?islem=eklendi");//boş kayıt eklenmesini engelleme
+}
+
+//SİLME KOMUTU
+if ($_REQUEST['islem'] == "sil")
+{
+    $id = $_REQUEST['id'];
+    $sql="DELETE FROM students WHERE Id=$id ";
+    $db->exec($sql);
+    header("Location: ?durum=silindi");
+}
+
+//DÜZENLEME KOMUTU
+if($_REQUEST['islem']=="duzenle" && $_SERVER['REQUEST_METHOD']=="POST")
+{
+    $id = $_REQUEST['id'];
+    $fullname = $_REQUEST['fullname'];
+    $department= $_REQUEST['department'];
+    $city = $_REQUEST['city'];
+    $sql = "UPDATE students SET fullname = '$fullname', department = '$department', city = '$city' WHERE Id = $id";
+    $db->exec($sql);
+    echo "<br> Düzenleme basarili";
+    header("Location: ?durum=duzenlendi");
+}
+
+?>
+
 
 <form action="?islem=ekle" method="post">
     Fullname: <input type="text" name="fullname" required><br>
@@ -40,31 +76,44 @@ try {
 </form>
 
 <?php
-//Ekleme Komutu
-if ($_REQUEST['islem']=="ekle"){
-    $fullname = $_REQUEST['fullname'];
-    $department = $_REQUEST['department'];
-    $city = $_REQUEST['city'];
-    $sql = "INSERT INTO students (fullname,department,city) VALUES ('$fullname', '$department','$city')";
-    $db->exec($sql);
-    echo "ADDED!!<br>";
-    header("Location: ?islem=eklendi");//boş kayıt eklenmesini engelleme
-}
+if($_REQUEST['islem']=="duzenle"){
+    $id = $_REQUEST['id'];
+    $sql = "SELECT * FROM students WHERE Id=$id";
+    $sonuc = $db->prepare($sql);
+    $sonuc->execute();
+    $data = $sonuc->fetch(PDO::FETCH_ASSOC);
 
-//SİLME KOMUTU
-if ($_REQUEST['islem']=="sil"){
-    $ID = $_REQUEST['ID'];
-    $sql = "DELETE FROM students WHERE ID=$ID";
-    $db->exec($sql);
-    header("Location: ?islem=silindi");
+    if ($sonuc->rowCount()>0){
+?>
+<form action="?islem=duzenle&id=<?= $data['Id']?>" method="POST">
+    Fullname <input type="text" name="fullname" value="<?=$data['fullname']?>"> <br>
+    Department <input type="text" name="department" value="<?=$data['department']?>"> <br>
+    City <select name="city">
+        <option><?=$data['department']?></option>
+        <option>Ankara</option>
+        <option>İstanbul</option>
+        <option>Bursa</option>
+        <option>Sinop</option>
+    </select>
+    <input type="submit" value="submit">
+</form>
+<?php
+    }
+    else{
+    echo "<br> Kayıt Bulunamadı";
+    }
 }
 ?>
+
+
 *******ÖĞRENCİ LİSTESİ******<br>
-<table style="border:1px solid">
+<table style="border:1px solid" width="500" bgcolor="#fff8dc">
     <tr>
+        <th>ID</th>
         <th>Full Name</th>
         <th>Department</th>
         <th>City</th>
+        <th>Edit</th>
         <th>Delete</th>
     </tr>
 <?php
@@ -74,11 +123,13 @@ foreach ($db->query($sql) as $data ){
 
 
 ?>
-    <tr>
+    <tr align="center">
+        <td><?=$data['id']?></td>
         <td><?=$data['fullname']?></td>
         <td><?=$data['department']?></td>
         <td><?=$data['city']?></td>
-        <td><a href="?islem = sil&id=<?=$data['ID']?>">X</a></td>
+        <td><a href="?islem=duzenle&id=<?=$data['id'] ?>"> Edit</a></td>
+        <td><a href="?islem = sil&id=<?=$data['id']?>" onclick="return confirm('are you sure?');">X</a></td>
 
     </tr>
     <?php
